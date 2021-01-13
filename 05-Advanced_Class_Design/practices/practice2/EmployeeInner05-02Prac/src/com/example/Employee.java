65b81da7ac400dad9c31c63f55f7b251da3ee900
tenico <tenico94@gmail.com>
2021-01-13 11:51:45 +0000

    Pratice 5.2

--- 05-Advanced_Class_Design/practices/practice2/EmployeeInner05-02Prac/src/com/example/Employee.java
+++ 05-Advanced_Class_Design/practices/practice2/EmployeeInner05-02Prac/src/com/example/Employee.java
@@ -6,11 +6,39 @@ public class Employee {
     private String role;
     private String dept;
     private double salary;
+    private BenefitsHelper BH = new BenefitsHelper();
+   
+    private class BenefitsHelper{
 
+        private final double bonusRate = 0.02;
+        private final double withholdingRate = 0.07;
+
+        protected double calcBonus(double salary){
+            return salary * bonusRate;
+        }
+
+        protected double calcWithholding(double salary){
+            return salary * withholdingRate ;
+        }
+    }
+    
+    
+   
+    
+    
+    public double getWithholding(){
+        return BH.calcWithholding(salary);
+    }
+    public double getBonus(){
+        return BH.calcBonus(salary);
+    }
+    
+    
     public Employee() {
         super();
+        
     }
-
+    
     public Employee(String name, String role, String dept, double salary) {
         this.name = name;
         this.role = role;
@@ -18,6 +46,7 @@ public class Employee {
         this.salary = salary;
     }
 
+    
     public String getName() {
         return name;
     }
