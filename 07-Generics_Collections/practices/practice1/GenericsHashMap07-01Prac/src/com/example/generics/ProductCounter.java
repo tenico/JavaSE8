84b7000efddb49dc5647bcba51fa75f5633e05e9
tenico <tenico94@gmail.com>
2021-01-14 11:44:57 +0000

    Practice 7-1

--- 07-Generics_Collections/practices/practice1/GenericsHashMap07-01Prac/src/com/example/generics/ProductCounter.java
+++ 07-Generics_Collections/practices/practice1/GenericsHashMap07-01Prac/src/com/example/generics/ProductCounter.java
@@ -1,12 +1,15 @@
 package com.example.generics;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProductCounter {
     // Create a Counting Map
     // Create a Name Mapping Map
+    private Map <String, Long> partNumbers = new ConcurrentHashMap<>();
+    private Map <String,String> productDescription = new TreeMap<>();
 
     public static void main(String[] args) {
 
@@ -19,19 +22,35 @@ public class ProductCounter {
         productNames.put("Black Polo Shirt", "1S02");
         productNames.put("Red Ball Cap", "1H01");
         productNames.put("Duke Mug   ", "1M02");
-
+        productNames.put("test","1414");
         // Create Product Counter Object and process data
+        System.out.println("=== Product Report ===");
+        ProductCounter product = new ProductCounter(productNames);
+        product.processList(parts);
+        product.printReport();
     }
 
-    public ProductCounter(Map productNames) {
-        // Your code here
+    public ProductCounter(Map<String,String> productNames) {
+        for (Entry<String,String> entry : productNames.entrySet()) {
+            productDescription.put(entry.getValue(), entry.getKey());
+        }
     }
 
     public void processList(String[] list) {
-        // your code here
+        for (String string : list) {
+            if (partNumbers.containsKey(string)){
+                   Long count = partNumbers.get(string);
+                   count ++;
+                   partNumbers.put(string, count);
+            } else {
+                partNumbers.put(string, 1L);
+            }
+        }
     }
 
     public void printReport() {
-        // Your code here
+       System.out.println("\n" + partNumbers);
+       System.out.println("\n"+ productDescription);
     }
+    
 }
