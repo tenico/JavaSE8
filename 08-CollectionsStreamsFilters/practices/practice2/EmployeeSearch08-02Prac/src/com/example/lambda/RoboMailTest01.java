4d5aec8e206b964b4301e97eb9f5104ac1bbf605
tenico <tenico94@gmail.com>
2021-01-17 17:09:22 +0000

    8.2 - Added parallels

--- 08-CollectionsStreamsFilters/practices/practice2/EmployeeSearch08-02Prac/src/com/example/lambda/RoboMailTest01.java
+++ 08-CollectionsStreamsFilters/practices/practice2/EmployeeSearch08-02Prac/src/com/example/lambda/RoboMailTest01.java
@@ -18,7 +18,7 @@ public class RoboMailTest01 {
     System.out.println("\n=== Sales Execs");
     
     // roboMail Sales Executives
-    pl.stream()
+    pl.stream().parallel()
     .filter(e -> e.getDept().equals("Sales"))
     .filter(e -> e.getRole().equals((Role.EXECUTIVE)))
     .forEach(robo::roboMail);
