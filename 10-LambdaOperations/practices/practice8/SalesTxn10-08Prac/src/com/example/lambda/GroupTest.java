4341db2bff271c0a5d2a71d1c307405b56081d13
tenico <tenico94@gmail.com>
2021-01-17 23:55:39 +0000

    Practice 10-8

--- 10-LambdaOperations/practices/practice8/SalesTxn10-08Prac/src/com/example/lambda/GroupTest.java
+++ 10-LambdaOperations/practices/practice8/SalesTxn10-08Prac/src/com/example/lambda/GroupTest.java
@@ -18,6 +18,12 @@ public class GroupTest {
         
         // Print out transactions grouped by Buyer
         System.out.println("=== Transactions Grouped by Buyer ===");
-        
+        tMap = tList.stream().parallel()
+        .collect(Collectors.groupingBy(SalesTxn::getBuyerName));
+    
+        tMap.forEach((k,v) -> {
+            System.out.println("\nBuyer: " + k);
+            v.forEach(SalesTxn::printSummary);
+        });
     }
 }
