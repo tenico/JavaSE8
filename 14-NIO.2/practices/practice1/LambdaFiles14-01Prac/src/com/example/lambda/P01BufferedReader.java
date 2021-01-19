f44d378dec1dbd1b633b07616db13a703c987a31
tenico <tenico94@gmail.com>
2021-01-19 11:09:20 +0000

    Practice 14-1

--- 14-NIO.2/practices/practice1/LambdaFiles14-01Prac/src/com/example/lambda/P01BufferedReader.java
+++ 14-NIO.2/practices/practice1/LambdaFiles14-01Prac/src/com/example/lambda/P01BufferedReader.java
@@ -3,6 +3,7 @@ package com.example.lambda;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  *
@@ -12,13 +13,18 @@ public class P01BufferedReader {
     
     public static void main(String[] args) {
         
+System.out.println(Path.of(".").toAbsolutePath());
+
         try{
             BufferedReader bReader = 
                 new BufferedReader(new FileReader("hamlet.txt"));
             
             System.out.println("=== Entire File ===");
             //print out file here
-                        
+            bReader.lines()
+                .forEach(line -> 
+                    System.out.println(line));
+                                
         }catch (IOException e){
             System.out.println("Error: " + e.getMessage());
         }
--- 14-NIO.2/practices/practice1/LambdaFiles14-01Prac/src/com/example/lambda/P02NioRead.java
+++ 14-NIO.2/practices/practice1/LambdaFiles14-01Prac/src/com/example/lambda/P02NioRead.java
@@ -12,15 +12,15 @@ import java.util.stream.Stream;
 public class P02NioRead {
     
     public static void main(String[] args) {
-/*  Remove these comments      
-        try{ // Create Try with Resources here
+
+
+        try(Stream<String> lines = Files.lines(Paths.get("hamlet.txt"))){
             
             System.out.println("\n=== Entire File ===");
-            // print lines here
+            lines.forEach(line -> System.out.println(line)); 
             
         }catch (IOException e){
             System.out.println("Error: " + e.getMessage());
         }
-*/
     } 
 }
