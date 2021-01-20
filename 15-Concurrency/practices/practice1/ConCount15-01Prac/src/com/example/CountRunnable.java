ea252da40c2075e8d5049e0539c09dad1d4d030d
tenico <tenico94@gmail.com>
2021-01-20 10:02:18 +0000

    Practice 15-1

--- 15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/CountRunnable.java
+++ 15-Concurrency/practices/practice1/ConCount15-01Prac/src/com/example/CountRunnable.java
@@ -2,12 +2,20 @@ package com.example;
 
 public class CountRunnable implements Runnable{
   
-   //final int count;
-  //final String threadName;
+  final int count;
+  final String threadName;
   
+  public CountRunnable(int count, String name){
+    this.count = count;
+    this.threadName = name;
+  }
   // Setup Constructor to initialize variables
   
   public void run(){ 
     // Count with for loop
+    for (int i = 1; i <= count; i++){
+      System.out.println("Thread " + threadName +
+        ": " + i);
+    }
   }
 }
